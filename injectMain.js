7f27b4807bd0f82f3ef695a10cacdcfe6a16bfff
Vitaly Sokolov <77505958+tivial19@users.noreply.github.com>
2024-05-13 17:29:14 +0300

    dd

---
+++ injectMain.js
@@ -0,0 +1,10 @@
+const siteRootUrl='https://tivial19.github.io/lampa/';
+const moduleUrl=siteRootUrl + 'main.js';
+//const moduleUrl='./main.js';
+
+const s = document.createElement('script');
+s.setAttribute('src', moduleUrl);
+s.setAttribute('type', 'module');
+document.body.appendChild(s);
+
+console.log('TiViAl', `Inject Module ${moduleUrl}`);
